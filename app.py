9c51be791e6742236e1928f382f47d9f33c100d1
Linda Fogli <foglilinda@gmail.com>
2024-03-27 13:15:38 +0100

    Login personal page, logout

--- app.py
+++ app.py
@@ -1,20 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from db_func import run_query
+import os
+
+
 
 app = Flask(__name__)
 
+app.secret_key = "psst"
+
+# TODO log status into decorator?
+def loggedin_check():
+    if session:
+        log_status = session['logged']
+        return log_status
+    else:
+        log_status = False
+        return log_status
+
 @app.route("/")
 def home():
-    return render_template('home.html')
+    log_status = loggedin_check()
+    return render_template('home.html', logged= log_status)
 
 @app.route("/register", methods=['GET'])
 def all_person_page():
+    log_status = loggedin_check()
+
+
     query = "SELECT * FROM person"
     data = run_query(query)
-    return render_template("all_person.html", data=data, searched=False)
+    return render_template("all_person.html", 
+                           data=data, searched=False, 
+                           logged=log_status)
 
 @app.route("/register", methods=['GET', 'POST'])
 def search_register():
+
+    log_status = loggedin_check()
+
+
     key = list(request.form.keys())[0]
     search_term = request.form.get(key)
 
@@ -28,40 +52,62 @@ def search_register():
         data = []
 
     if data != []:
-        return render_template("all_person.html", searched_data=data, searched=True)
+        return render_template("all_person.html", searched_data=data, searched=True, 
+                           logged= log_status)
     else:
-        return render_template("all_person.html", searched_data=data, searched=True)
+        return render_template("all_person.html", searched_data=data, searched=True, 
+                           logged= log_status)
+    
 
 @app.route("/register/<int:id>", methods=['GET'])
 def pers_info(id):
     query = f"SELECT * FROM person WHERE personID like {id}"
     data = run_query(query)
-    return render_template("pers_info.html", data=data[0])
+
+    log_status = loggedin_check()
+
+    return render_template("pers_info.html", data=data[0], logged= log_status)
+
 
 
 
 
 @app.route("/login")
 def login_page():
-    return render_template("login.html")
+    log_status = loggedin_check()
+    return render_template("login.html", logged= log_status)
+
 
 @app.route("/login", methods=["POST"])
 def login_validation():
-    form_pw, form_user = request.form["login_pw"], request.form["login_user"]    
 
-    query = f"SELECT * FROM user WHERE username LIKE '{form_user}'"
-    data = run_query(query)
+    if "Logout" in request.form:
+        session['logged'] = False
 
-    if data:
-        data_pw, data_user = data[0]['password'], data[0]['username']
+    try:
 
-        if form_pw == data_pw:
-            logged= True
-            return render_template('home.html', logged = True)
-        else: 
+        form_pw, form_user = request.form["login_pw"], request.form["login_user"]    
+
+        query = f"SELECT * FROM user WHERE username LIKE '{form_user}'"
+        data = run_query(query)
+
+        if data:
+            data_pw, data_user = data[0]['password'], data[0]['username']
+
+            if form_pw == data_pw:
+                session['logged'] = True
+                return render_template('home.html', logged = session['logged'])
+            else: 
+                return render_template("login.html", error="Wrong username or password!")
+        else:
             return render_template("login.html", error="Wrong username or password!")
-    else:
-        return render_template("login.html", error="Wrong username or password!")
+        
+    except Exception as e:
+        print(e)
+
+    return render_template('home.html', logged = session['logged'])
+    
+
     
 
 
@@ -72,6 +118,7 @@ def login_validation():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
 
